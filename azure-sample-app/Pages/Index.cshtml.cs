fd69e4abe232b33e4cd831d4c39ba9e36b792745
Rambabu <m.rambabu4u@gmail.com>
2023-07-28 21:35:53 +0530

    configured message

--- azure-sample-app/Pages/Index.cshtml.cs
+++ azure-sample-app/Pages/Index.cshtml.cs
@@ -6,14 +6,16 @@ namespace azure_sample_app.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly IConfiguration _configuration;
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
+            ViewData["Greetings"] = _configuration["Greetings"];
 
         }
     }
